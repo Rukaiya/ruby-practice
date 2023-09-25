365ab61796a7873912b5788b50969f2948d2e38d
Rukaiya <rukaiyaamin36@gmail.com>
2023-09-25 17:58:30 +0600

    updated test.rb

--- test.rb
+++ test.rb
@@ -1,2 +1,18 @@
-max_result = nil
-puts max_result
+def cal_points(ops)
+  temp = []
+  ops.each do |op|
+    case op
+    when op.to_i.to_s == op
+      temp << op.to_i
+    when 'D'
+      temp << op.to_i * 2
+    when 'C'
+      temp.pop
+    when '+'
+      temp << temp[-1].to_i + temp[-2].to-i
+    end
+    temp.sum
+  end
+end
+
+puts cal_points(['5', '2', 'C', 'D', '+'])
